4f7b6ca682afbb035739ab55a6f7566093654f6b
Raider <gabrielnae1@gmail.com>
2022-11-26 15:56:04 +0100

    Typescript Environment Configuration

---
+++ src/Test.ts
@@ -0,0 +1,8 @@
+import * as readlineSync from "readline-sync";
+
+console.log("hello World")
+let number = 10
+console.log(number+1)
+
+let annee_nombre = Number(readlineSync.question("Quel est ton age ?"))
+console.log("voici ton age",annee_nombre)
