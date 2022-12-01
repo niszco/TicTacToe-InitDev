792d09b7e215dfee73f19e22d3fb71b1e285f026
niszco <nico.marszalek@gmail.com>
2022-12-01 09:25:29 +0100

    Test push laptop

--- src/Test.ts
+++ src/Test.ts
@@ -1,10 +1,11 @@
 import * as readlineSync from "readline-sync";
 
-console.log("hello World")
-let number = 10
-console.log(number+1)
+console.log("hello World");
+let number = 10;
+console.log(number + 1);
 
-let annee_nombre = Number(readlineSync.question("Quel est ton age ?"))
-console.log("voici ton age",annee_nombre)
+let annee_nombre = Number(readlineSync.question("Quel est ton age ?"));
+console.log("voici ton age", annee_nombre);
 
 let n = 3;
+let m = 4;
